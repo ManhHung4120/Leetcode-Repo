00ebc726e85f1761646fa0903c3d85394d5d13c1
ManhHung4120 <94128344+ManhHung4120@users.noreply.github.com>
2024-11-20 21:05:17 +0700

    Time: 11 ms (5.76%) | Memory: 18.1 MB (9.27%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -10,15 +10,12 @@ class Solution:
         if not len(nums):
             return None
         middle_index = math.floor(len(nums)/2)
-        print(middle_index)
         left = [nums[i] for i in range (0, middle_index)]
         right = [nums[j] for j in range (middle_index + 1, len(nums))]
-
         root = TreeNode(nums[middle_index])
         if len(left):
             root.left = self.sortedArrayToBST(left)
         if len(right):
             root.right = self.sortedArrayToBST(right)
-        print(root)
         return root
         
